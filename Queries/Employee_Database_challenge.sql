098af4007017457c83613537142ed8c2cb34a352
David Fashbinder <david.fashbinder@gmail.com>
2020-11-06 10:55:42 -0500

    adding deliverable 1 and 2, as well as readme outline

---
+++ Queries/Employee_Database_challenge.sql
@@ -0,0 +1,56 @@
+DROP TABLE retirement_titles
+
+SELECT e.emp_no,
+	e.first_name,
+	e.last_name,
+	ts.title,
+	ts.from_date,
+	ts.to_date
+INTO retirement_titles
+FROM employees as e
+INNER JOIN titles as ts
+ON e.emp_no = ts.emp_no
+WHERE e.birth_date BETWEEN '1952-01-01' AND '1955-12-31'
+ORDER BY emp_no;
+
+SELECT * FROM retirement_titles
+
+-- Use Dictinct with Orderby to remove duplicate rows
+SELECT DISTINCT ON (rt.emp_no) rt.emp_no,
+rt.first_name,
+rt.last_name,
+rt.title
+INTO unique_titles
+FROM retirement_titles as rt 
+ORDER BY rt.emp_no, rt.to_date DESC;
+
+SELECT * FROM unique_titles
+
+DROP TABLE retiring_titles
+
+SELECT COUNT(ut.title), 
+	ut.title
+INTO retiring_titles
+FROM unique_titles as ut
+GROUP BY ut.title
+ORDER BY ut.count DESC;
+
+SELECT * FROM retiring_titles
+
+SELECT DISTINCT ON (e.emp_no) e.emp_no,
+e.first_name,
+e.last_name,
+e.birth_date,
+de.from_date,
+de.to_date,
+ts.title
+INTO mentorship_eligibility
+FROM employees as e
+INNER JOIN dept_emp as de
+ON e.emp_no = de.emp_no
+INNER JOIN titles as ts
+ON e.emp_no = ts.emp_no
+WHERE e.birth_date BETWEEN '1965-01-01' AND '1965-12-31'
+ORDER BY emp_no
+
+SELECT * FROM mentorship_eligibility
